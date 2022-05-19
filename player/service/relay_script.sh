433454b507cba603030c19b56a9387e8246d7bdb
Henrik Frisk <mail@henrikfrisk.com>
2022-05-19 11:29:45 +0200

    Added services for the scripts and for pd

---
+++ player/service/relay_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/python -u /home/pi/Documents/rain/biosound/player/python_server_relay.py >>/home/pi/py_log.log 1>&1
--- player/test.py
+++
@@ -1,5 +0,0 @@
-import re
-
-new_string = 'gate 37;\n'
-new_result = re.findall('[0-9]+', new_string)
-print(new_result[0])
