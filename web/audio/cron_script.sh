2ee729a58493c31324762df30bd9695da3367153
Henrik Frisk <mail@henrikfrisk.com>
2022-05-22 13:43:18 +0200

    Updated record script

--- web/audio/cron_script.sh
+++ web/audio/cron_script.sh
@@ -1,5 +1,5 @@
 script="/home/pi/Documents/rain/biosound/player/cron/record_script.sh"
-#  chmod +x $script
+chmod +x $script
 job="0 8,16 * * * $script > /home/pi/cron_log.log 2>&1"
 echo "$job"
 # remove the previous version of this cron job if it exists
