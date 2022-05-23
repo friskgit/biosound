279e4e2fa32389960ece4c83054f25df7912d840
Henrik Frisk <mail@henrikfrisk.com>
2022-05-23 09:53:51 +0200

    Change reverb

--- web/audio/record_script.sh
+++ web/audio/record_script.sh
@@ -12,7 +12,7 @@ arecord -D plughw:1,0 -f cd -c 4 -d $sec $file_name
 echo "Creating stereo file"
 sox $file_name output_stereo.wav remix 1v0.9723,2v0.80901,3v0.5877,4v0.233445 1v0.23344,2v0.5877,3v0.80901,4v0.97236
 echo "High pass filter, reverb and fades"
-sox output_stereo.wav filtered_stereo.wav channels 2 highpass 600 pad 0 3 reverb 50 50 75 fade 1
+sox output_stereo.wav filtered_stereo.wav channels 2 highpass 600 pad 0 3 reverb 10 50 75 fade 1
 echo "Encode the file to mp3"
 lame --preset medium filtered_stereo.wav $mp3_name
 echo "Upload the file to server"
